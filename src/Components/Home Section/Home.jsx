20a4814f310b20695e5c51ce89021142435a29ca
samadmemon930 <samadbusiness93@gmail.com>
2025-12-29 10:58:27 +0500

    update

--- src/Components/Home Section/Home.jsx
+++ src/Components/Home Section/Home.jsx
@@ -13,8 +13,7 @@ const Home = () => {
         <div className="home-text">
           <h1>We Ensure better education for a better world</h1>
           <p>
-            Our cutting-edge curriculum is designed to empower students
-            with the knowledge and skills needed to excel
+            Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education
           </p>
 
           <Link to = "/" className="btn">
